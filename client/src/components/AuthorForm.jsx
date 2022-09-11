7ebca27824080e8b224d543eebaa4b2126911f26
brandontaylor156 <brandon.david4@outlook.com>
2022-09-10 19:06:07 -0700

    authors full CRUD

---
+++ client/src/components/AuthorForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ProductForm = (props) => {
+    const {initialName, onSubmitProp} = props;
+    const [name, setName] = useState(initialName); 
+
+    const navigate = useNavigate()
+
+    const onSubmitHandler = e => {
+        e.preventDefault();
+        onSubmitProp({name})
+    }
+    
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <p>
+                <label>Name:</label><br/>
+                <input type="text" onChange={(e)=>setName(e.target.value)} value={name}/>
+            </p>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '5px'}}>
+                <button type="submit">Submit</button>
+                <button onClick={()=>navigate('/authors')}>Cancel</button>
+            </div>
+        </form>
+    )
+}
+
+export default ProductForm
+
---
+++ server/controllers/author.controller.js
@@ -0,0 +1,41 @@
+const { Author } = require('../models/author.model');
+
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+
+module.exports.findAllAuthors = (request, response) => {
+    Author.find()
+      .then(authorList => response.json(authorList))
+      .catch(err => response.json({ message: "Something went wrong", error: err }))
+  };
+
+module.exports.createAuthor = (request, response) => {
+    const { name } = request.body;
+    Author.create({
+        name
+    })
+        .then(author => response.json(author))
+        .catch(err => response.status(400).json(err));
+}
+
+module.exports.getAuthor = (request, response) => {
+    Author.findOne({_id:request.params.id})
+        .then(author => {response.json(author)})
+        .catch(err => response.json(err))
+}
+
+module.exports.updateAuthor = (request, response) => {
+    Author.findOneAndUpdate({_id:request.params.id}, request.body, {new:true, runValidators: true})
+        .then(updatedAuthor => response.json(updatedAuthor))
+        .catch(err => response.status(400).json(err))
+}
+
+module.exports.deleteAuthor = (request, response) => {
+    Author.deleteOne({ _id: request.params.id })
+        .then(deleteConfirmation => response.json(deleteConfirmation))
+        .catch(err => response.json(err))
+}
+
